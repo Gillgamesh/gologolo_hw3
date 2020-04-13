bd7dd5ec08aa9f169e672275a2dcb1bb6edd7711
Gillgamesh <gilvirgilgg@gmail.com>
2020-04-12 20:31:02 -0400

    Fixed creating logos, redirected user to /view

--- client/src/components/CreateLogoScreen.jsx
+++ client/src/components/CreateLogoScreen.jsx
@@ -3,6 +3,8 @@ import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import { Link } from 'react-router-dom';
 import {ADD_LOGO} from '../queries';
+import {DEFAULT_LOGO} from '../constants';
+import LogoEditor from'./LogoEditor';
 
 
 class CreateLogoScreen extends Component {
@@ -10,7 +12,12 @@ class CreateLogoScreen extends Component {
     render() {
         let text, color, fontSize;
         return (
-            <Mutation mutation={ADD_LOGO} onCompleted={() => this.props.history.push('/')}>
+            <Mutation
+                mutation={ADD_LOGO}
+                onCompleted={(data) => this.props.history.push(
+                    `/view/${data.addLogo._id}`
+                )}
+            >
                 {(addLogo, { loading, error }) => (
                     <div className="container">
                         <div className="panel panel-default">
@@ -18,38 +25,21 @@ class CreateLogoScreen extends Component {
                                 <h4><Link to="/">Home</Link></h4>
                                 <h3 className="panel-title">
                                     Create Logo
-                            </h3>
+                                        </h3>
                             </div>
                             <div className="panel-body">
-                                <form onSubmit={e => {
-                                    e.preventDefault();
-                                    addLogo({ variables: { text: text.value, color: color.value, fontSize: parseInt(fontSize.value) } });
-                                    text.value = "";
-                                    color.value = "";
-                                    fontSize.value = "";
-                                }}>
-                                    <div className="form-group">
-                                        <label htmlFor="text">Text:</label>
-                                        <input type="text" className="form-control" name="text" ref={node => {
-                                            text = node;
-                                        }} placeholder="Text" />
+                                <div className="panel-body">
+                                    <div className="container">
+                                        {loading && <p>Loading...</p>}
+                                        {error && <p>Error :( Please try again)</p>}
+                                        <LogoEditor
+                                            logo={DEFAULT_LOGO}
+                                            submit={(newLogo) => {
+                                                addLogo({ variables: { ...newLogo } })
+                                            }}
+                                        />
                                     </div>
-                                    <div className="form-group">
-                                        <label htmlFor="color">Color:</label>
-                                        <input type="color" className="form-control" name="color" ref={node => {
-                                            color = node;
-                                        }} placeholder="Color" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label htmlFor="fontSize">Font Size:</label>
-                                        <input type="number" className="form-control" name="fontSize" ref={node => {
-                                            fontSize = node;
-                                        }} placeholder="Font Size" />
-                                    </div>
-                                    <button type="submit" className="btn btn-success">Submit</button>
-                                </form>
-                                {loading && <p>Loading...</p>}
-                                {error && <p>Error :( Please try again</p>}
+                                </div>
                             </div>
                         </div>
                     </div>
